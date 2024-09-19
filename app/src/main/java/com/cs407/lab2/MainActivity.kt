2cb3a574b9aa0a2f55d8d432bf37043fb025661e
dylanzulkosky <dylanzulkosky@icloud.com>
2024-09-19 16:55:36 -0500

    Added text line enter

--- app/src/main/java/com/cs407/lab2/MainActivity.kt
+++ app/src/main/java/com/cs407/lab2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.cs407.lab2
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        submitButton.setOnClickListener {
+            Log.i("INFO", "Button Clicked!")
         }
     }
 }
